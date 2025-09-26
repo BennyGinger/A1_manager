61d3e6c7977bedecafa87095b59de30573c6e600
Benoit Roux <benoit.roux@gmail.com>
2025-09-26 16:45:13 +0200

    Refactor head_api and marZ_api for improved clarity and functionality; update volume handling and add detailed docstrings.

--- a1_manager/microscope_hardware/nanopick/head_api.py
+++ a1_manager/microscope_hardware/nanopick/head_api.py
@@ -1,146 +1,131 @@
-
 from __future__ import annotations # Enable type annotation to be stored as string
+from dataclasses import dataclass
 import logging
 
 import requests
 
 from a1_manager.microscope_hardware.nanopick.marZ_api import MarZ
-from utils.utility_classes import StageCoord # Do I need the center position since the stage movement is handled by the Nikon class?
+from utils.utility_classes import StageCoord # Do I need the center position since the stage movement is handled by the Nikon class? No I don't think so, since this will be handled by the higher library (e.g. gem-screening). Once you see that message, please remove the import
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
+# TODO: Check default value
 BASE_URL = "http://localhost:5000"
 
 # Volumes
-FILL_VOLUME = 500
-INJECT_VOLUME = 70
-MIXING_VOLUME = 50
-MAX_VOLUME = 600
+MIXING_VOLUME = 50 # in nanoliters
+MIXING_TIME = 20 # in milliseconds
+MAX_VOLUME = 600 # in nanoliters
 
 # Movement of the head
 MOVING_UP = -33000
 MOVING_DOWN = 33000
 
+
+# NOTE: For now I removed some methods, like flushing, we need to experiment with the API more before doing anything.
+@dataclass(slots=True)
 class Head():
-    """Class that controls the API head.
     """
-    __slots__ = 'arm', '_track_volume'
-    
-    def __init__(self, arm: MarZ) -> None:
-        self.arm = arm
-        self._track_volume = 0  # in nanoliters
-    
+    Class that controls the API head.
+    """
+    arm: MarZ
+    _track_volume: float = 0  # in nanoliters
+
     @property
     def track_volume(self) -> float:
         return self._track_volume
     
-    # TODO: Check time default value
-    def set_volume(self, volume: float, time: float = 100) -> None:
-        """ A volume-time pair is sent to the controller. The piezo unit will start immediately 
-            to withdraw or inject the specified volume under the specified time. The volume values 
-            are absolute values. If the volume is less than the previously sent item, then fluid is 
-            withdrawn through the pipette. If the volume is greater than the previously sent one, fluid 
-            will be injected back."""        
+    def _set_volume(self, volume: float, time: float = 100) -> None:
+        """
+        A volume-time pair is sent to the controller. The piezo unit will start immediately to withdraw or inject the specified volume under the specified time. The volume values are absolute values. If the volume is less than the previously sent item, then fluid is withdrawn through the pipette. If the volume is greater than the previously sent one, fluid will be injected back.
+        
+        Args:
+            volume (float): Volume in nanoliters (positive value to inject, negative value to withdraw)
+            time (float): Time in milliseconds (default: 100 ms)
+        """
 
-        #Endpoint and parameters
+        # Endpoint and parameters
         endpoint = f"{BASE_URL}/setVolume"
-        params = {
-            "volume": volume,   # example volume in nanoliters
-            "time": time    # example time in milliseconds
-        }
+        params = {"volume": volume,   # example volume in nanoliters
+                    "time": time}    # example time in milliseconds
         try:
             response = requests.put(endpoint, params=params)
-
             if response.status_code == 200:
-                logger.debug("Success:", response.text)
+                logger.debug(f"Success: {response.text}")
             else:
-                logger.error(f"Error {response.status_code}:", response.text)
+                logger.error(f"Error {response.status_code}: {response.text}")
         except requests.exceptions.RequestException as e:
-            logger.error("Request failed:", e)
+            logger.error(f"Request failed: {e}")
     
     def set_LED(self, ID: int, brightness: int) -> None:
-        """Set brightness level of LED """
-        #Endpoint and parameters
+        """
+        Set brightness level of LED 
+        
+        Args:
+            ID (int): LED ID (1-2)
+            brightness (int): Brightness level (0-100)
+        """
+        # Endpoint and parameters
         endpoint = f"{BASE_URL}/setLED"
-        params = {
-            "ID": ID,   # example volume in nanoliters
-            "brightness": brightness }   # example time in milliseconds
+        params = {"ID": ID,   # example volume in nanoliters
+                "brightness": brightness}   # example time in milliseconds
         try:
             response = requests.put(endpoint, params=params)
 
             if response.status_code == 200:
-                logger.debug("Success:", response.text)
+                logger.debug(f"Success: {response.text}")
             else:
-                logger.error(f"Error {response.status_code}:", response.text)
+                logger.error(f"Error {response.status_code}: {response.text}")
         except requests.exceptions.RequestException as e:
-            logger.error("Request failed:", e)
+            logger.error(f"Request failed: {e}")
 
-    def _update_volume(self, volume: float, state: str) -> float:
-            if state == "fill":
-                self._track_volume += volume
-            
-            if state == "inject":
-                self._track_volume += volume
-                
-            if state == "flush":
-                self._track_volume -= volume
+    # FIXME: If you have time, can you check the volume handleing by the api, because I think -vol is sucking up and +vol is pushing out, but I am not 100% sure. This needs to be then changed accordingly in the code (i.e. in the injecting and filling function)
+    def filling(self, volume: float, time: float = 100) -> None:
+        """
+        Fill the pipette with a specified volume of liquid. If the requested volume exceeds the maximum capacity of the pipette, it will be capped at MAX_VOLUME.
+        """
+        if volume + self.track_volume >= MAX_VOLUME:
+            vol_to_fill = MAX_VOLUME - self.track_volume
+            logger.warning(f"The volume to fill exceeds the maximum volume of the pipette! It will be set to {MAX_VOLUME} nanoliters (i.e. maximum).")
+        else:
+            vol_to_fill = volume
+
+        # Move down the head to reach the liquid
+        self.arm.set_arm_position(MOVING_DOWN)
+
+        # Draw the liquid into the pipette
+        self._set_volume(vol_to_fill, time)
+        self._track_volume += vol_to_fill
+
+        # Move up the head
+        self.arm.set_arm_position(MOVING_UP)
+
+    def injecting(self, volume: float, time: float = 100, mixing_cycles: int = 3) -> None:
+        """
+        Inject a specified volume of liquid from the pipette. If the requested volume exceeds the current volume in the pipette, it will be capped at the current volume.
+        """
+        if volume > self.track_volume:
+            logger.warning(f"The volume to inject exceeds the current volume in the pipette! It will be set to {self.track_volume} nanoliters (i.e. current volume).")
+            vol_to_inject = self.track_volume
+        else:
+            vol_to_inject = volume   
         
-    # TODO: Check flush default volume     
-    def flushing(self) -> None: 
-        # Get all the fluid out of the pipette       
-        self.set_volume(self.track_volume)
-        self.update_volume(self.track_volume, "flush")
-    
-    # TODO: Check filling default volume 
-    def filling(self, volume: float = FILL_VOLUME) -> None:              
-        # Check if the pipette is empty, if not, then flush it out
-        if self.track_volume != 0:
-            self.flushing()
-        # Check if the filling does not cause an overflow
-        if self.track_volume + volume >= MAX_VOLUME:
-            logger.warning("The pipette is about to fill up! Please choose a new value between zero and %s", (self.track_volume + volume -5))
-        else:
-            # Move down the head to reach the liquid
-            self.Arm.set_head_position(MOVING_DOWN)
-                    
-            # Draw the liquid into the pipette        
-            self.set_volume(volume)
-            self.update_volume(volume, "fill")
-                    
-            # Move up the head
-            self.Arm.set_head_position(MOVING_UP) 
-    
-    # TODO: Check inject default volume 
-    def injecting(self, volume: float = INJECT_VOLUME) -> None: 
-        # Check if there is enough liquid to inject   
-        if self.track_volume < volume:  
-            logger.warning("There is not enough liquid in the pipette. Please refill it!")
-        else:             
-            # Move down the head to reach the liquid
-            self.Arm.set_head_position(MOVING_DOWN)
-                    
-            # Draw the liquid into the pipette        
-            self.set_volume(volume)
-            self.update_volume(volume, "inject")
-                    
-            # Move up the head
-            self.Arm.set_head_position(MOVING_UP) 
-    
-    # TODO: Check mixing default volume
-    def mixing(self, n: int, volume: float = MIXING_VOLUME):
-        # Check if the pipette is empty, if not, then flush it out
-        if self.track_volume != 0:
-            self.flushing()
-        else:
-            # Move down the head to reach the liquid
-            self.Arm.set_head_position(MOVING_DOWN)
-                    
-            # Draw the liquid into the pipette 
-            # 'n': number of mixing cycles
-            for i in range(n):       
-                self.set_volume(volume) # suck it up
-                self.set_volume(-volume) # let it out
-                    
-            # Move up the head
-            self.Arm.set_head_position(MOVING_UP) 
+        # Move down the head to reach the liquid
+        self.arm.set_arm_position(MOVING_DOWN)
+        
+        # Draw the liquid into the pipette
+        self._set_volume(vol_to_inject, time)
+        self._mixing(mixing_cycles)
+        self._track_volume -= vol_to_inject
+
+        # Move up the head
+        self.arm.set_arm_position(MOVING_UP)
+
+    def _mixing(self, n: int):
+        """
+        Mix the liquid in the pipette by sucking it up and letting it out multiple times.
+        """
+        for _ in range(n):
+            self._set_volume(MIXING_VOLUME, MIXING_TIME)  # suck it up
+            self._set_volume(-MIXING_VOLUME, MIXING_TIME)  # let it out
--- a1_manager/microscope_hardware/nanopick/marZ_api.py
+++ a1_manager/microscope_hardware/nanopick/marZ_api.py
@@ -1,7 +1,8 @@
 from __future__ import annotations # Enable type annotation to be stored as string
-from pycromanager import Core
 import logging
+from dataclasses import dataclass
 
+from pycromanager import Core
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -10,28 +11,37 @@ MAX_HEIGHT = 7000
 MIN_HEIGHT = -7000
 
 SAFE_HEIGHT = 3500 # The stage controller has to wait until the head reaches this altitude - will be used 
-
+@dataclass(slots=True)
 class MarZ:
-    """Class to control the movement of the arm (Märzhäuser controller)."""
-    __slots__ = 'core', 'destination'
-    
-    def __init__(self, core: Core, destination: float) -> None:
-        self.core = core
-        self.destination = destination
-    
+    """
+    Class to control the movement of the arm (Märzhäuser controller).
+    """
+    core: Core
+    destination: float
+
     @property
     def get_arm_position(self)->float:
-        """Get the current altitude of the head."""
+        """
+        Get the current altitude of the head.
+        """
         return self.core.get_position('ZAxis') # type: ignore
         
     def set_arm_position(self, destination: float)->None:
-        """ Move the head to the desired position. """
+        """
+        Move the head to the desired position.
+        Args:
+            destination (float): The target altitude for the head movement (in range -7000 to 7000, arbitrary units).
+        """
         if destination > MIN_HEIGHT and destination < MAX_HEIGHT:
             self.core.set_position('ZAxis', destination) # type: ignore
+            self._safety_waiting()
         else:
             logger.warning("The destination is out of range! Please choose a value between %s and %s", MIN_HEIGHT, MAX_HEIGHT)
         
-    def safety_waiting(self):
+    def _safety_waiting(self):
+        """
+        Wait until the head reaches a safe height.
+        """
         while True:
             height = self.get_arm_position
             if height >= SAFE_HEIGHT:
