e52d76b0aba35e9e5a02cea6283d00a2b8c8615d
ZsuzsannaRohan <rohan.zsuzsanna.t@gmail.com>
2025-10-23 14:44:19 +0200

    Update test_head.py

--- a1_manager/microscope_hardware/nanopick/test_head.py
+++ a1_manager/microscope_hardware/nanopick/test_head.py
@@ -2,26 +2,39 @@ from a1_manager import A1Manager, StageCoord, launch_dish_workflow
 import numpy as np
 
 
-def fov_stimulate(self, data: dict) -> tuple[list, list, list]:  
+def fov_stimulate(self, data: dict, pattern: str) -> tuple[list, list, list]:  
     x = [] 
     y = []   
     for _, coord in data.items():
         x.append(coord["xy"][0])
         y.append(coord["xy"][1])
-    
-    fov_1 = [7, 6, 5, 11, 20, 21, 22, 9, 10] # upper left
-    fov_2 = [37, 36, 35, 51, 52, 53, 54, 49, 50] # (quite) in the middle 
-    fov_3 = [27, 28, 29, 38, 48, 47, 46, 40, 39] # lower right
-    fov1 = []
-    for i in range(len(fov_1)):
-        fov1.append([x[fov_1[i]], y[fov_1[i]]])
-    fov2 = []
-    for i in range(len(fov_2)):
-        fov2.append([x[fov_2[i]], y[fov_2[i]]])
-    fov3 = []
-    for i in range(len(fov_3)):
-        fov3.append([x[fov_3[i]], y[fov_3[i]]])
-    return fov1, fov2, fov3
+    if pattern == "3x3":
+        fov_1 = [7, 6, 5, 11, 20, 21, 22, 9, 10] # upper left
+        fov_2 = [37, 36, 35, 51, 52, 53, 54, 49, 50] # (quite) in the middle 
+        fov_3 = [27, 28, 29, 38, 48, 47, 46, 40, 39] # lower right
+        fov1 = []
+        for i in range(len(fov_1)):
+            fov1.append([x[fov_1[i]], y[fov_1[i]]])
+        fov2 = []
+        for i in range(len(fov_2)):
+            fov2.append([x[fov_2[i]], y[fov_2[i]]])
+        fov3 = []
+        for i in range(len(fov_3)):
+            fov3.append([x[fov_3[i]], y[fov_3[i]]])
+        return fov1, fov2, fov3
+
+
+    if pattern == "5x5":
+        # 5*5 fov
+        fov_5_5 = [8, 9, 10, 11, 12, 19, 29, 38, 48, 47, 46, 45, 44, 42, 25, 23, 22, 21, 20, 28, 39, 40, 41, 26, 27]
+        fov5 = []
+        for i in range(len(fov_5_5)):
+                  fov5.append([x[fov_5_5[i]], y[fov_5_5[i]]])
+        fov_3 = [27, 28, 29, 38, 48, 47, 46, 40, 39] # lower right
+        fov3 = []
+        for i in range(len(fov_3)):
+                 fov3.append([x[fov_3[i]], y[fov_3[i]]])
+        return fov3, fov5
     
 
 if __name__ == "__main__":
@@ -68,7 +81,7 @@ if __name__ == "__main__":
     a1_manager.filling(volume=500)
 
     # prepare the subgrid to stimulate
-    subgrid_lst = fov_stimulate(grid[well_selection])
+    subgrid_lst = fov_stimulate(grid[well_selection], "3x3")
     
     for i, xy_coords in enumerate(subgrid_lst[subgrid_instance]):
         stage_coord = StageCoord()
@@ -122,17 +135,6 @@ if __name__ == "__main__":
     
     
     
-    # if pattern == "5x5":
-    #     # 5*5 fov
-    #     fov_5_5 = [8, 9, 10, 11, 12, 19, 29, 38, 48, 47, 46, 45, 44, 42, 25, 23, 22, 21, 20, 28, 39, 40, 41, 26, 27]
-    #     fov5 = []
-    #     for i in range(len(fov_5_5)):
-              fov5.append([x[fov_5_5[i]], y[fov_5_5[i]]])
-          fov_3 = [27, 28, 29, 38, 48, 47, 46, 40, 39] # lower right
-          fov3 = []
-          for i in range(len(fov_3)):
-             fov3.append([x[fov_3[i]], y[fov_3[i]]])
-           return fov3, fov5
 
     #     fov_5_5_center = [x[center_5_5], y[center_5_5]]
     #     fov_5_5_ctr = data[well][str(center_5_5)]["xy"]
