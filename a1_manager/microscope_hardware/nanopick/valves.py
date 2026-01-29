74fda12db598e2cc3e319b36be8566b4d1bfde2b
benoit roux <benoit.roux@gmail.com>
2026-01-29 13:36:48 +0100

    Update PICController initialization and enhance optical configuration settings

--- a1_manager/microscope_hardware/nanopick/valves.py
+++ a1_manager/microscope_hardware/nanopick/valves.py
@@ -1,6 +1,5 @@
 from __future__ import annotations # Enable type annotation to be stored as string
 import logging
-from turtle import position
 from a1_manager.a1manager import A1Manager
 import serial
 import time
@@ -207,7 +206,7 @@ if __name__ == "__main__":
      
     """
 
-    controller = PICController(needle_size=50, pressure=0.3)
+    controller = PICController(needle_size=50, pressure=0.2)
     # from a1_manager import A1Manager
     # from a1_manager import StageCoord
     # a1_manager = A1Manager(objective = '10x')
@@ -228,39 +227,63 @@ if __name__ == "__main__":
     from time import sleep
     from typing import Any
     from a1_manager import A1Manager, StageCoord
-
-     
-    a1_manager = A1Manager(objective='10x')
-    dish_calib_path = Path(r"C:\Users\uManager\Documents\__repos__\GEM_suite\A1_manager\config\calib_96well.json")
-    with open(dish_calib_path, 'r') as f:
-        dish_calib: dict[str, dict[str, Any]]= json.load(f)
-
-    keys = [
-        #   'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
-        #  'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12',
-        #  'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12',
-          'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
-        #      'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12',
-        #  'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12',
-        #     'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',
-        #  'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'
-             ]
     
-    print("Wells in calibration:", keys)
+    from tifffile import imwrite
+
+
+    save_dir = Path(r"C:\Users\uManager\Desktop\test")
+    a1_manager = A1Manager(objective='20x')
+    for i in range(5):
+        # channel 1
+        a1_manager.oc_settings(optical_configuration='GFP')
+        img = a1_manager.snap_image()
+        imwrite(save_dir / f"channel1_image_{i}.tif", img)
+        
+        # channel 2
+        a1_manager.oc_settings(optical_configuration='405GFP')
+        img2 = a1_manager.snap_image()
+        imwrite(save_dir / f"channel2_image_{i}.tif", img2)
+        
+        if i == 3:
+            continue
+            # controller.injecting(inject_vol_ul=10, mixing_cycles=1)
+        
+        
+    
+    
+    # #dish_calib_path = Path(r"C:\Users\uManager\Documents\__repos__\GEM_suite\A1_manager\config\calib_96well.json")
+    # #with open(dish_calib_path, 'r') as f:
+    # #    dish_calib: dict[str, dict[str, Any]]= json.load(f)
+
+    # keys = ['A7'
+    #     #   'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
+    #     #  'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12',
+    #     #  'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12',
+    #     #  'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
+    #     #      'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12',
+    #     #  'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12',
+    #     #     'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',
+    #     #  'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'
+    #          ]
+    
+    # #print("Wells in calibration:", keys)
    
-    for well in list(keys):
-         print(well)
+    # for well in list(keys):
+    #      print(well)
        
 
-         mt = dish_calib.get(well, {})
-         position = StageCoord(xy=mt['center'])
-         a1_manager.set_stage_position(position)
-         sleep(7)   
+    #      #mt = dish_calib.get(well, {})
+    #      #position = StageCoord(xy=mt['center'])
+    #      #a1_manager.set_stage_position(position)
+    #      #sleep(5)   
+    #     # Image before stimulation
+ 
+        
        
-         # Injection of ligands
+    #      # Injection of ligands
          
-         controller.injecting(inject_vol_ul=10, mixing_cycles=1)
+    #      controller.injecting(inject_vol_ul=10, mixing_cycles=1)
 
-         sleep(7)
+    #      #sleep(7)
    
     controller._close() 
