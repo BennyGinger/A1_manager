f87eed2e369e08648c1c217c74f140ef5bc52936
benoit roux <benoit.roux@gmail.com>
2026-02-10 13:28:03 +0100

    refactor: Comment out unused calibration code and update save directory path

--- a1_manager/microscope_hardware/nanopick/valve.py
+++ a1_manager/microscope_hardware/nanopick/valve.py
@@ -215,27 +215,27 @@ if __name__ == "__main__":
     
     from typing import Any
     from a1_manager import A1Manager, StageCoord
-    dish_calib_path = Path(r"C:\Users\uManager\Documents\__repos__\GEM_suite\A1_manager\config\calib_96well.json")
-    with open(dish_calib_path, 'r') as f:
-       dish_calib: dict[str, dict[str, Any]]= json.load(f)
+    # dish_calib_path = Path(r"C:\Users\uManager\Documents\__repos__\GEM_suite\A1_manager\config\calib_96well.json")
+    # with open(dish_calib_path, 'r') as f:
+    #    dish_calib: dict[str, dict[str, Any]]= json.load(f)
 
-    keys = ['B6'
-        #   'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
-        #  'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12',
-        #  'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12',
-        #  'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
-        #      'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12',
-        #  'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12',
-        #     'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',
-        #  'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'
-             ]
-    mt = dish_calib.get(keys[0], {})
-    position = StageCoord(xy=mt['center'])
+    # keys = ['B6'
+    #     #   'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
+    #     #  'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12',
+    #     #  'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12',
+    #     #  'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
+    #     #      'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12',
+    #     #  'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12',
+    #     #     'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',
+    #     #  'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12'
+    #          ]
+    # mt = dish_calib.get(keys[0], {})
+    # position = StageCoord(xy=mt['center'])
     a1_manager = A1Manager(objective='20x', lamp_name='pE-800', )
-    a1_manager.set_stage_position(position)
+    # a1_manager.set_stage_position(position)
     
     controller = PICController(needle_size=50, pressure=0.2)
-    save_dir = Path(r"C:\Users\uManager\Desktop\test")
+    save_dir = Path(r"C:\Users\uManager\Desktop\test\first")
     a1_manager = A1Manager(objective='20x', lamp_name='pE-800', )
     for i in range(10):
         # channel 1
