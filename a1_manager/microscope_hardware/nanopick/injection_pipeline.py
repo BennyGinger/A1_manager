79e9bf84f0e0e1ed6ec65fd1aafae24ca5f181bc
ZsuzsannaRohan <rohan.zsuzsanna.t@gmail.com>
2026-03-02 14:59:25 +0100

    Update injection_pipeline.py

--- a1_manager/microscope_hardware/nanopick/injection_pipeline.py
+++ a1_manager/microscope_hardware/nanopick/injection_pipeline.py
@@ -7,7 +7,9 @@ from tifffile import imwrite
 from time import sleep
 from typing import Any
 
-from a1_manager.microscope_hardware.nanopick.injection_factory import Injection
+from a1_manager.microscope_hardware.nanopick.devices.marZ import MarZ
+from a1_manager.microscope_hardware.nanopick.devices.injection_device import Init_Device
+from a1_manager.microscope_hardware.nanopick.injection_factory import Inject
 from a1_manager import A1Manager
 
 # Set up logging
@@ -19,10 +21,11 @@ if __name__ == "__main__":
         Automated stimulation protocol using the valve system. 
         
         """
-    
-        master = Injection(dish_name = "96-well", injection_volume_ul = 10, injection_time_ms = None)   
-        injection_device = master.get_injection_device(injection_device = 'quickpick', needle_size = 50, pressure=0.3) 
+        arm = MarZ(core=a1_manage.core, dish='96well')
+        injection_device = Init_Device(injection_device = 'quickpick') 
+        injection_device.set_quick_params(needle_size = 50, pressure = 0.2)
         a1_manager = A1Manager(objective = '10x', lamp_name = 'pE-800')
+        master = Inject(arm = arm, injection_device = injection_device, a1_manager = a1_manager)
 
         run_dir = Path('D:\\Zsuzsi\\inj_pipeline_test')
         dish_calib = {}
@@ -47,7 +50,7 @@ if __name__ == "__main__":
              imwrite(run_dir / img_name, img, compression='zlib')
         
              # Injection of ligands
-             injection_device.inject(master.injection_volume_ul)
+             master.injection_device.inject(10)
              master.arm_to_liquid() # Dip it in the liquid because of the drops.
              master.arm_to_home()
              sleep(1)
